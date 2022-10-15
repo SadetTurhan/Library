1316094c8ed304ed012b2e0457a16f1e97af620d
Sadet Turhan <msadetturhan1@gmail.com>
2022-10-15 23:47:15 +0300

    finallyworking

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 let myLibrary = [];
-let bookname = document.getElementById('name').value
-let bookauthor = document.getElementById('author').value
+
 let booksadded = document.getElementById('[data-id="book1"]')
 const add = document.querySelector('[data-id="add"]')
 const addbutton = document.getElementById('add')
@@ -8,33 +7,38 @@ const erasebutton = document.getElementById('erase')
 const erase = document.querySelector('[data-id="erase"]')
 const addedbooks = document.getElementsByClassName('addedbook')
 
-function Book() {
-  this.name = bookname;
-  this.author = bookauthor;
-}
+var Book = function() {
+    let name = bookname;
+    let author = bookauthor;
+  };
 
 function addBookToLibrary() {
-    addbutton.addEventListener('click', (event) => {
-        myLibrary.innerHTML = myLibrary.push(bookname);
-        myLibrary.innerHTML = myLibrary.push(bookauthor);
+    addbutton.addEventListener('click', () => {
+        myLibrary.pop(0);
+        myLibrary.pop(1);
+        var bookname = document.getElementById('name').value
+        var bookauthor = document.getElementById('author').value
+        myLibrary[name] = myLibrary.push(bookname);
+        myLibrary[author] = myLibrary.push(bookauthor); 
         });
-}
+};
 
-addBookToLibrary()
 
 function showbook() {
-    addbutton.addEventListener('click', (event) => {
-        const newDiv = document.createElement("div");
-        newDiv.setAttribute('id','newbook');
-     const newContent = document.createTextNode(myLibrary);
-     newDiv.append(newContent);
-    const currentDiv = document.getElementById("newbook");
+    addbutton.addEventListener('click', () => {
+    const newDiv = document.createElement("div");
+    newDiv.setAttribute('id','newbook');
+    const newContent = document.createTextNode(myLibrary);
+    newDiv.append(newContent);
+    const currentDiv = document.getElementById('[data-id="book1"]');
     document.body.insertBefore(newDiv, currentDiv);
-    })
+    });
 }
 
-showbook()
+addBookToLibrary();
+showbook();
 
 erasebutton.addEventListener('click', (event) => {
     erasebutton.textContent = `Click count: ${event.detail}`;
 });
+
