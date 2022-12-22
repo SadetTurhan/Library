7bb7a04743ee407c61900c03897c198139a9f817
Sadet Turhan <msadetturhan1@gmail.com>
2022-12-22 19:01:35 +0300

    some functionality

--- app.js
+++ app.js
@@ -7,10 +7,40 @@ function Book(title,author,pages,read){
   this.read = read
 };
 
-function addBookToLibrary(Book) {
-  const BookInfo = Object.values(Book)
-  myLibrary.push(BookInfo);
+function addBookToLibrary(Book){
+  myLibrary.pop();
+  myLibrary.pop();
+  myLibrary.pop();
+  myLibrary.pop();
+  myLibrary.push(bookName,bookAuthor,bookPages,bookRead);
 };
 
-const book1 = new Book("a",2,"3","4");
-addBookToLibrary(book1)
+
+function addCard(){
+    let card = document.createElement("div");
+    let cardName = document.createElement("p");
+    cardName.innerText = document.getElementById("name_of_book").value;
+    let cardAuthor = document.createElement("p");
+    cardAuthor.innerText = document.getElementById("author_of_book").value;
+    let cardPage = document.createElement("p");
+    cardPage.innerText = document.getElementById("page_number").value;
+    let cardRead = document.createElement("p");
+    if(cardRead == "on"){
+        cardRead.innerText = "has been read";
+    }else{
+        cardRead.innerText = "has not been read";
+    }
+    card.appendChild(cardName);
+    card.appendChild(cardAuthor);
+    card.appendChild(cardPage);
+    card.appendChild(cardRead);
+    document.body.appendChild(card);
+};
+
+
+
+
+let addButton = document.getElementById("addButton");
+addButton.addEventListener("click", addBookToLibrary);
+addButton.addEventListener("click",addCard);
+
