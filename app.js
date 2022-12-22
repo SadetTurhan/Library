9e786c80b9334d9083a020d33f5c9cfa9c9972f3
Sadet Turhan <msadetturhan1@gmail.com>
2022-12-22 18:03:43 +0300

    obj constructors added

--- app.js
+++ app.js
@@ -1,44 +1,16 @@
 let myLibrary = [];
 
-let booksadded = document.getElementById('[data-id="book1"]')
-const add = document.querySelector('[data-id="add"]')
-const addbutton = document.getElementById('add')
-const erasebutton = document.getElementById('erase')
-const erase = document.querySelector('[data-id="erase"]')
-const addedbooks = document.getElementsByClassName('addedbook')
-
-var Book = function() {
-    let name = bookname;
-    let author = bookauthor;
-  };
-
-function addBookToLibrary() {
-    addbutton.addEventListener('click', () => {
-        myLibrary.pop(0);
-        myLibrary.pop(1);
-        var bookname = document.getElementById('name').value
-        var bookauthor = document.getElementById('author').value
-        myLibrary[name] = myLibrary.push(bookname);
-        myLibrary[author] = myLibrary.push(bookauthor); 
-        });
+function Book(title,author,pages,read){
+  this.title = title,
+  this.author = author,
+  this.pages = pages,
+  this.read = read
 };
 
+function addBookToLibrary(Book) {
+  const BookInfo = Object.values(Book)
+  myLibrary.push(BookInfo);
+};
 
-function showbook() {
-    addbutton.addEventListener('click', () => {
-    const newDiv = document.createElement("div");
-    newDiv.setAttribute('id','newbook');
-    const newContent = document.createTextNode(myLibrary);
-    newDiv.append(newContent);
-    const currentDiv = document.getElementById('[data-id="book1"]');
-    document.body.insertBefore(newDiv, currentDiv);
-    });
-}
-
-addBookToLibrary();
-showbook();
-
-erasebutton.addEventListener('click', (event) => {
-    erasebutton.textContent = `Click count: ${event.detail}`;
-});
-
+const book1 = new Book("a",2,"3","4");
+addBookToLibrary(book1)
