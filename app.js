a413669b94b182c766d4bdb53138171eb80349ae
Sadet Turhan <msadetturhan1@gmail.com>
2023-02-05 18:03:15 +0300

    working with local storage still has bugs

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-let myLibrary = [];
-
+const inputName = document.getElementById("name_of_book");
+const inputAuthor = document.getElementById("author_of_book");
+const inputPages = document.getElementById("page_number");
+const inputRead = document.getElementById("read_status");
 
 function Book(title,author,pages,read){
   this.title = title;
@@ -8,53 +10,105 @@ function Book(title,author,pages,read){
   this.read = read;
  };
 function addBookToLibrary(title,author,pages,read){
-  var title = document.getElementById("name_of_book").value;
-  var author = document.getElementById("author_of_book").value;
-  var pages = document.getElementById("page_number").value;
-  var read = document.getElementById("read_status").checked;
+  let myLibrary = [];
+  var title = inputName.value;
+  var author = inputAuthor.value;
+  var pages = inputPages.value;
+  var read = inputRead.value;
   const book = new Book(title,author,pages,read);
   myLibrary.push(book);
 }
-
-  function addCard(){
-  let card = document.createElement("div");
-  card.classList.add("card");
-  let cardName = document.createElement("p");
-  cardName.innerText = myLibrary.slice(-1)[0].title;
-  cardName.setAttribute("id","items1");
-  let cardAuthor = document.createElement("p");
-  cardAuthor.innerText = myLibrary.slice(-1)[0].author;
-  cardAuthor.setAttribute("id","items2");
-  let cardPage = document.createElement("p");
-  cardPage.innerText = myLibrary.slice(-1)[0].pages;
-  let cardRead = document.createElement("p");
-  cardPage.setAttribute("id","items3");
-  if(myLibrary.slice(-1)[0].read === true){
-    cardRead.innerText = "Has been read";
-  }else{
-    cardRead.innerText = "Has not been read";
-  };
-  cardRead.setAttribute("id","items4");
-  card.appendChild(cardName);
+function createCard(){
+  const card = document.createElement("div");
+  card.setAttribute("id","card");
+  const cardTitle = document.createElement("p");
+  const cardAuthor = document.createElement("p");
+  const cardPages = document.createElement("p");
+  const cardRead = document.createElement("p");
+  cardTitle.textContent = inputName.value;
+  card.appendChild(cardTitle);
+  cardAuthor.textContent = inputAuthor.value;
   card.appendChild(cardAuthor);
-  card.appendChild(cardPage);
+  cardPages.textContent = inputPages.value;
+  card.appendChild(cardPages);
+  cardRead.textContent = inputRead.value;
   card.appendChild(cardRead);
-  let cardArea = document.getElementById("cardArea");
-  document.body.appendChild(card);
+  const cardArea = document.getElementById("cardArea");
   cardArea.appendChild(card);
-  
-  
-  let deleteButton = document.createElement("button");
-  deleteButton.classList.add("deleteButton");
-  deleteButton.innerText = "delete";
+  const deleteButton = document.createElement("button");
   card.appendChild(deleteButton);
-function deleteBook(){
-  card.remove();
+  deleteButton.addEventListener("click",removeLocalTodos);
+  deleteButton.addEventListener("click",deleteCard);
+  saveLocalBook(inputAuthor.value);
+  saveLocalBook(inputName.value);
+  saveLocalBook(inputPages.value);
+  saveLocalBook(inputRead.value);
+  inputName.value = "";
+  inputAuthor.value = "";
+  inputPages.value = "";
+  inputRead.value = "";
 }
-  deleteButton.addEventListener("click",deleteBook);
-};
-
-let addButton = document.getElementById("addButton");
+function deleteCard(){
+  const card = document.getElementById("card");
+  card.innerText = "";
+}
+const addButton = document.getElementById("addButton");
 addButton.addEventListener("click", addBookToLibrary);
-addButton.addEventListener("click",addCard);
+addButton.addEventListener("click",createCard);
+document.addEventListener("DOMContentLoaded",getBook);
+function saveLocalBook(book){
+  //Check 
+  let myLibrary;
+  if (localStorage.getItem("myLibrary") === null){
+      myLibrary = [];
+  }else{
+      myLibrary = JSON.parse(localStorage.getItem("myLibrary"));
+  }
+  myLibrary.push(book);
+  localStorage.setItem("myLibrary",JSON.stringify(myLibrary));
+}
 
+function removeLocalTodos(todo){
+  let myLibrary;
+  if (localStorage.getItem("myLibrary") === null){
+      myLibrary = [];
+  }else{
+      myLibrary = JSON.parse(localStorage.getItem("myLibrary"));
+  }
+  const libraryIndex = myLibrary[0].innerText;
+  myLibrary.splice(myLibrary.indexOf(libraryIndex),1);
+  localStorage.setItem("myLibrary",JSON.stringify(myLibrary));
+}
+
+function getBook(){
+  let myLibrary;
+  if (localStorage.getItem("myLibrary") === null){
+      myLibrary = [];
+  }else{
+      myLibrary = JSON.parse(localStorage.getItem("myLibrary"));
+  }
+  myLibrary.forEach(function(book){
+    const card = document.createElement("div");
+    card.classList.add("card");
+    const cardTitle = document.createElement("p");
+    const cardAuthor = document.createElement("p");
+    const cardPages = document.createElement("p");
+    const cardRead = document.createElement("p");
+    cardTitle.textContent = inputName.value;
+    card.appendChild(cardTitle);
+    cardAuthor.textContent = inputAuthor.value;
+    card.appendChild(cardAuthor);
+    cardPages.textContent = inputPages.value;
+    card.appendChild(cardPages);
+    cardRead.textContent = inputRead.value;
+    card.appendChild(cardRead);
+    const cardArea = document.getElementById("cardArea");
+    cardArea.appendChild(card);
+    const deleteButton = document.createElement("button");
+    card.appendChild(deleteButton);
+    deleteButton.addEventListener("click",removeLocalTodos);
+    inputName.value = "";
+    inputAuthor.value = "";
+    inputPages.value = "";
+  })
+}
